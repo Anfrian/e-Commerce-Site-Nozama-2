dc4ed6ab8768f684b1ca84a509f133fe7917b4db
Anfrian <gorkemkadir14@hotmail.com>
2022-07-23 02:58:09 +0300

    Cart, Login & Register pages

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,222 @@
+import React from 'react'
+import styled from "styled-components";
+import { Add, Remove } from "@material-ui/icons";
+import Announcement from '../components/Announcement';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+
+const Container = styled.div`
+
+`;
+const Wrapper = styled.div`
+    padding: 20px;
+`;
+const Title = styled.h1`
+    font-weight: 300;
+    text-align: center;
+`;
+const Top = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+`;
+const TopButton = styled.button`
+    padding: 10px;
+    font-weight: 600;
+    cursor: pointer;
+    border: ${props=> props.type === "filled" && "none"};
+    background-color: ${props=> props.type === "filled" ? "black" : "transparent"};
+    color: ${props=> props.type === "filled" && "white"};
+`;
+const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+const TopText = styled.span`
+    text-decoration: underline;
+    cursor: pointer;
+    margin: 0px 10px;
+`;
+const TopTexts = styled.div`
+
+`;
+const Info = styled.div`
+    flex: 3;
+`;
+const Summary = styled.div`
+    flex: 1;
+    background-color: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    height: 50vh;
+`;
+
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+`;
+const ProductDetail = styled.div`
+    flex: 2;
+    display: flex;
+`;
+const Image = styled.img`
+    width: 200px;
+`;
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+const ProductName = styled.span`
+    
+`;
+const ProductID = styled.span`
+    
+`;
+const ProductColor = styled.div`
+    width:20px;
+    height:20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+`;
+const ProductSize = styled.span`
+    
+`;
+const PriceDetail = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`;
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`;
+const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 200;
+`;
+const Hr = styled.hr`
+    background-color: #eee;
+    border: none;
+    height: 1px;
+`;
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+const SummaryItemText = styled.span``;
+const SummaryItemPrice = styled.span``;
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
+
+const Cart = () => {
+  return (
+    <Container>
+        <NavBar></NavBar>
+        <Announcement></Announcement>
+        <Wrapper>
+            <Title>Cart</Title>
+            <Top>
+                <TopButton>
+                    Continue
+                </TopButton>
+                <TopTexts>
+                    <TopText>Cart</TopText>
+                    <TopText>Wishlist</TopText>
+                </TopTexts>
+                <TopButton type="filled">
+                    Checkout
+                </TopButton>
+            </Top>
+            <Bottom>
+                <Info>
+                    <Product>
+                        <ProductDetail>
+                            <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A"/>
+                            <Details>
+                                <ProductName><b>Product: </b>Shoes</ProductName>
+                                <ProductID><b>ID: </b>2435543</ProductID>
+                                <ProductColor color="black"/>
+                                <ProductSize><b>Size: </b>37.5</ProductSize>
+                            </Details>
+                        </ProductDetail>
+                        <PriceDetail>
+                            <ProductAmountContainer>
+                                <Add></Add>
+                                <ProductAmount>2</ProductAmount>
+                                <Remove></Remove>
+                            </ProductAmountContainer>
+                            <ProductPrice>$ 30</ProductPrice>
+                        </PriceDetail>
+                    </Product>
+                    <Hr>
+                    </Hr>
+                    <Product>
+                        <ProductDetail>
+                            <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A"/>
+                            <Details>
+                                <ProductName><b>Product: </b>Shoes</ProductName>
+                                <ProductID><b>ID: </b>2435543</ProductID>
+                                <ProductColor color="black"/>
+                                <ProductSize><b>Size: </b>37.5</ProductSize>
+                            </Details>
+                        </ProductDetail>
+                        <PriceDetail>
+                            <ProductAmountContainer>
+                                <Add></Add>
+                                <ProductAmount>2</ProductAmount>
+                                <Remove></Remove>
+                            </ProductAmountContainer>
+                            <ProductPrice>$ 30</ProductPrice>
+                        </PriceDetail>
+                    </Product>
+                </Info>
+                <Summary>
+                    <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                    <SummaryItem>
+                        <SummaryItemText>Subtotal</SummaryItemText>
+                        <SummaryItemPrice>$ 80</SummaryItemPrice>
+                    </SummaryItem>
+                    <SummaryItem>
+                        <SummaryItemText>Estimated Shipping</SummaryItemText>
+                        <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+                    </SummaryItem>
+                    <SummaryItem>
+                        <SummaryItemText>Shipping Discount</SummaryItemText>
+                        <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+                    </SummaryItem>
+                    <SummaryItem type="total">
+                        <SummaryItemText>Total</SummaryItemText>
+                        <SummaryItemPrice>$ 80</SummaryItemPrice>
+                    </SummaryItem>
+                    <Button>Checkout Now</Button>
+                </Summary>
+            </Bottom>
+        </Wrapper>
+        <Footer></Footer>
+    </Container>
+  )
+}
+
+export default Cart
